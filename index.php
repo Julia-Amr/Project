279953225bf4d5e2c39e87a5fe6ec43ac27a9aeb
Júlia <Jlia@192.168.1.2>
2026-02-16 21:38:04 +0100

    Add updated index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 // Kódováni jazyk UTF-8
 mb_internal_encoding("UTF-8");
-// Načtení URL adresáře pro načtení CSS souboru
-define('BASE_URL', '/');
 // Načtení kontroleru
 require 'kontrolery/Kontroler.php';
